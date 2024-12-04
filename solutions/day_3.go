fde7dd5f1180c9a659c3fc91a87fe0aab470d070
John Cuviello <jcuviell@uci.edu>
2024-12-04 02:43:34 -0800

    added solution 3.2

--- solutions/day_3.go
+++ solutions/day_3.go
@@ -14,7 +14,6 @@ func get_input() string {
     }
 
     input_str := ""
-
     file, err := os.Open(os.Args[1])
     if err != nil {
         fmt.Println("Error")
@@ -36,13 +35,30 @@ func find_commands(arg_string string) []string {
     return matches
 }
 
+func find_commands_2(arg_string string) []string {
+    commands := []string{}
+    r, _ := regexp.Compile(`do\(\)(.*?)don't\(\)`)
+    initial_matches := r.FindAllString(arg_string, -1)
+
+    r_2, _ := regexp.Compile(`(.*?)don't\(\)`)
+    beginning_matches := r_2.FindAllString(arg_string, 1)
+    initial_matches = append(initial_matches, beginning_matches...)
+
+    r_3, _ := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)`)
+    for _, match_str := range initial_matches {
+        new_matches := r_3.FindAllString(match_str, -1)
+        commands = append(commands, new_matches...)
+    }
+    return commands
+
+}
+
 func calculate_commands(matches []string) int {
     total := 0
     expression := `\d{1,3}`
     r, _ := regexp.Compile(expression)
     for _, val := range matches {
         nums := r.FindAllString(val, -1)
-        fmt.Println(nums[0], nums[1])
         num_1, _ := strconv.Atoi(nums[0])
         num_2, _ := strconv.Atoi(nums[1])
         total += num_1 * num_2
@@ -52,9 +68,14 @@ func calculate_commands(matches []string) int {
 }
 
 func main() {
-    //example_arg_string := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    // example_arg_string := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    // example_arg_string_2 := "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
     arg_string := get_input()
-    matches := find_commands(arg_string)
-    fmt.Println(calculate_commands(matches))
+    // Part 1:
+    // matches := find_commands(arg_string)
+    // fmt.Println(calculate_commands(matches))
+    // Part 2:
+    matches_2 := find_commands_2(arg_string)
+    fmt.Println(calculate_commands(matches_2))
 
 }
