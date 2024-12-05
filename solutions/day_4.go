2d45ee734ff804c69b0173edde3328efd688a9e3
John Cuviello <jcuviell@uci.edu>
2024-12-04 17:08:33 -0800

    solution for day 4.2

--- solutions/day_4.go
+++ solutions/day_4.go
@@ -60,6 +60,28 @@ func is_diag_valid_backwards(matrix []string, row int, col int) bool {
 
 }
 
+func is_diag_valid_mas(matrix []string, row int, col int) bool {
+    curr_string := ""
+    for i := 0; i < 3; i++ {
+        curr_string += string(matrix[row + i][col + i])
+    }
+    if curr_string != "MAS" && curr_string != "SAM" {
+        return false
+    }
+    curr_string = ""
+    for i := 0; i < 3; i++ {
+        curr_string += string(matrix[row + i][col + 2 - i])
+    }
+    if curr_string == "MAS" || curr_string == "SAM" {
+        return true
+    }
+    return false
+    
+
+    
+    return false
+}
+
 func get_xmas_count(matrix []string) int {
     xmas_count := 0
     for i, row := range matrix {
@@ -87,7 +109,22 @@ func get_xmas_count(matrix []string) int {
     return xmas_count
 }
 
+func get_mas_x_count(matrix []string) int {
+    mas_x_count := 0
+    for i, row := range matrix {
+        for j, val := range matrix[i] { 
+            // diag top left to bottom right
+            if (val == 'M' || val == 'S') && 
+                j + 3 <= len(row) && i + 3 <= len(matrix) && 
+                is_diag_valid_mas(matrix, i, j) {
+                mas_x_count += 1
+            }
+         }
+    }
+    return mas_x_count
+}
 func main() {
     input_matrix := get_input()
-    fmt.Println(get_xmas_count(input_matrix))
+    fmt.Println("Part 1:", get_xmas_count(input_matrix))
+    fmt.Println("Part 2:", get_mas_x_count(input_matrix))
 }
